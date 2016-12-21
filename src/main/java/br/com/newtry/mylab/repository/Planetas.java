bef2b6017ad793104c60a4e1096421f24540f726
Nysio <nysiojunior@gmail.com>
2016-12-21 09:17:35 -0200

    Remover arquivos desnecessarios

--- src/main/java/br/com/newtry/mylab/repository/Planetas.java
+++
@@ -1,9 +0,0 @@
-package br.com.newtry.mylab.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.newtry.mylab.model.Planeta;
-
-public interface Planetas extends JpaRepository<Planeta, Long> {
-
-}
